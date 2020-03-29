81e12ec235d260b0eaa2ba9deda7506d9e457466
Kris Brown <mouserbrown@googlemail.com>
2020-03-29 17:38:34 +0100

    Updates

---
+++ precache-manifest.d22cd54706fa84afb9f0542ea552904e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9e2ac48aab7e73a1c5088147cfccaf",
+    "url": "/react-multipages/index.html"
+  },
+  {
+    "revision": "bab2308a46c6fef5b5d3",
+    "url": "/react-multipages/static/css/main.6db49686.chunk.css"
+  },
+  {
+    "revision": "c960d222fa465986d750",
+    "url": "/react-multipages/static/js/2.283fd842.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-multipages/static/js/2.283fd842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab2308a46c6fef5b5d3",
+    "url": "/react-multipages/static/js/main.e8fae16a.chunk.js"
+  },
+  {
+    "revision": "20ef79ae7f322cbfb80c",
+    "url": "/react-multipages/static/js/runtime-main.21264fe8.js"
+  }
+]);
